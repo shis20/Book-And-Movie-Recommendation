f60779cd9bd660535a33073aa0f2b5b7405902ab
Joyce1008Shuting <99380263+Joyce1008Shuting@users.noreply.github.com>
2022-02-10 12:25:56 +0800

    upload the component files folder

---
+++ Book and Movie database/components/movie/index.js
@@ -0,0 +1,30 @@
+// components/movie/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        movie:Object
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onGoToDetail(event){
+          // console.log(this.properties.movie)
+          const mid = this.properties.movie.id
+          wx.navigateTo({
+            url: '/pages/movie-detail/movie-detail?mid=' + mid
+          })
+        }
+      }
+    })
+    
---
+++ Book and Movie database/components/post/index.js
@@ -0,0 +1,27 @@
+// components/post/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        text:{
+            type:String,
+            value:'123'
+        }
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
