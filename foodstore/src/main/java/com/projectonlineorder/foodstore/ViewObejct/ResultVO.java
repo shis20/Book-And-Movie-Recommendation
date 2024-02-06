24e054a7fa7136df390f0917e5bc692dcdd82cc6
Shuting Shi <99380263+shis20@users.noreply.github.com>
2024-02-06 15:36:37 -0500

    Delete foodstore directory

--- foodstore/src/main/java/com/projectonlineorder/foodstore/ViewObejct/ResultVO.java
+++
@@ -1,9 +0,0 @@
-package com.projectonlineorder.foodstore.ViewObejct;
-import lombok.Data;
-
-@Data
-public class ResultVO {
-    private Integer code;
-    private String msg;
-    private T data;
-}
